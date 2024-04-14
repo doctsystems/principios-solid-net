d91fd33f216676df8e87611a7b1a6c51af49ce0c
Diego Osvaldo <doctsystems@gmail.com>
2024-04-14 18:40:44 -0400

    first commit
    proyecto de inicio

---
+++ PrincipiosSOLID/PrincipiosSOLID/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrincipiosSOLID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        
+        public Beer(string name, string brand)
+        {
+            Name = name;
+            Brand = brand;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine($"Cerveza {Name} - {Brand}, guardada");
+        }
+
+        public void Send()
+        {
+            Console.WriteLine($"Enviando cerveza {Name} - {Brand}");
+        }
+
+    }
+}
